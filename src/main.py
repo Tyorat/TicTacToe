cf44dea2dfd12b88309d62688a6c7196ebf6540d
Sviatoslav <you@example.com>
2021-06-22 14:15:01 +0300

    Create check for turn and riese error if wrong request

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 """
 Game Tic-Tac-Toe
 """
+import random
 import socket
 import sys
 
@@ -14,31 +15,56 @@ WIN_COMBO = ((1, 2, 3),
              (7, 5, 3)
              )
 
+SWITCH_TURN = {"x": "o", "o": "x"}
+
+
+class WrongMove(Exception): pass
+
 
 class Game:
     def __init__(self):
         self.__field = list(range(1, 10))
+        self.__turn = None
+        self.choose_random_player()
+
+    turn = property()
+
+    @turn.getter
+    def turn(self):
+        return self.__turn
 
     def check_end_game(self):
+        self.show_field()
         for combo in WIN_COMBO:
             if self.__field[combo[0] - 1] == self.__field[combo[1] - 1] == self.__field[combo[2] - 1]:
                 return f"win {self.__field[combo[0] - 1]}"
-            else:
-                print(self.__field)
 
     def check_turn(self, index, who):
-        if self.__field[index - 1] == index and who in "xo" and any(map(lambda x: str(x).isdigit(), self.__field)):
-            self.__field[index - 1] = who
-            self.check_end_game()
-        else:
-            raise Exception("invalid turn")
+        if self.__field[index - 1] != index:
+            raise WrongMove("The cell is already occupied")
+        elif who not in "xo":
+            raise WrongMove("Wrong symbol")
+        elif who != self.__turn:
+            raise WrongMove("Not your turn")
+        self.__field[index - 1] = who
+        self.check_end_game()
+        self.__turn = SWITCH_TURN[self.__turn]
+
+    def choose_random_player(self):
+        self.__turn = random.choice("xo")
+
+    def show_field(self):
+        print("************")
+        print("|" + "|".join(map(str, self.__field[:3])) + "|")
+        print("|" + "|".join(map(str, self.__field[3:6])) + "|")
+        print("|" + "|".join(map(str, self.__field[6:])) + "|")
+        print("************")
 
 
 if __name__ == "__main__":
     game = Game()
-    game.check_turn(1, "x")
-    game.check_turn(2, "o")
-    game.check_turn(3, "x")
-    game.check_turn(5, "o")
-    game.check_turn(8, "o")
-    print(game.check_end_game())
+    # for num in range(1, 10):
+    #     game.check_turn(num, "x")
+    while True:
+        game.check_turn(int(input()), input())
+    # print(game.check_end_game())
